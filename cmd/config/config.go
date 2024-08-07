3b8b538c87e29f5221c58d09ff66c1d54388b54f
ilya <sol1kek@estelink.ru>
2024-08-07 23:51:18 +0300

    fixx

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -11,6 +11,6 @@ var (
 
 func ParseFlags() {
 	flag.StringVar(&FlagRunAddr, "a", ":8080", "address and port to run server")
-	flag.StringVar(&FlagBaseURL, "b", "http://localhost:8000", "base URL for shortened links")
+	flag.StringVar(&FlagBaseURL, "b", "http://localhost:8080", "base URL for shortened links")
 	flag.Parse()
 }
