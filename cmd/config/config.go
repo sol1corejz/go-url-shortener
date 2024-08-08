cf1a311a8184ff7d0dc4b8a8d5855ac5b37b72f4
ilya <sol1kek@estelink.ru>
2024-08-08 17:24:12 +0300

    iter5

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"os"
 )
 
 var (
@@ -10,7 +11,16 @@ var (
 )
 
 func ParseFlags() {
+
 	flag.StringVar(&FlagRunAddr, "a", ":8080", "address and port to run server")
 	flag.StringVar(&FlagBaseURL, "b", "http://localhost:8080", "base URL for shortened links")
 	flag.Parse()
+
+	if envRunAddr := os.Getenv("SERVER_ADDRESS"); envRunAddr != "" {
+		FlagRunAddr = envRunAddr
+	}
+
+	if envBaseURL := os.Getenv("BASE_URL"); envBaseURL != "" {
+		FlagBaseURL = envBaseURL
+	}
 }
