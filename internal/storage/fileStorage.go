1cb0b20527a54ea5737ba0dffc8e8d715eb7dffd
ilya <sol1kek@estelink.ru>
2024-09-12 00:09:17 +0300

    fix

--- internal/storage/fileStorage.go
+++ internal/storage/fileStorage.go
@@ -1,82 +1,76 @@
 package storage
 
 import (
-	"encoding/json"
 	"fmt"
+	"github.com/sol1corejz/go-url-shortener/internal/file"
 	"github.com/sol1corejz/go-url-shortener/internal/models"
-	"os"
 	"sync"
 )
 
 type FileStorage struct {
 	filename string
 	mu       sync.Mutex
-	data     []models.URLData
+	data     map[string]models.URLData
+	producer *file.Producer
+	consumer *file.Consumer
 }
 
 func NewFileStorage(filename string) (*FileStorage, error) {
 	fs := &FileStorage{
 		filename: filename,
-		data:     make([]models.URLData, 0),
+		data:     make(map[string]models.URLData),
 	}
 
-	file, err := os.OpenFile(filename, os.O_RDONLY|os.O_CREATE, 0666)
+	producer, err := file.NewProducer(filename)
 	if err != nil {
 		return nil, err
 	}
-	defer file.Close()
+	fs.producer = producer
 
-	err = json.NewDecoder(file).Decode(&fs.data)
-	if err != nil && err.Error() != "EOF" {
+	consumer, err := file.NewConsumer(filename)
+	if err != nil {
 		return nil, err
+	}
+	fs.consumer = consumer
+
+	for {
+		event, err := fs.consumer.ReadEvent()
+		if err != nil {
+			break
+		}
+		fs.data[event.ShortURL] = models.URLData{
+			UUID:        event.UUID,
+			ShortURL:    event.ShortURL,
+			OriginalURL: event.OriginalURL,
+		}
 	}
 
 	return fs, nil
 }
 
-func (fs *FileStorage) saveToFile() error {
-	file, err := os.OpenFile(fs.filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	err = json.NewEncoder(file).Encode(fs.data)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (fs *FileStorage) Save(data models.URLData) error {
-	fs.mu.Lock()
-	urlData := models.URLData{
+func (fs *FileStorage) saveToFile(data models.URLData) error {
+	event := &file.Event{
 		UUID:        data.UUID,
 		ShortURL:    data.ShortURL,
 		OriginalURL: data.OriginalURL,
 	}
+	return fs.producer.WriteEvent(event)
+}
+
+func (fs *FileStorage) Save(data models.URLData) error {
+	fs.mu.Lock()
 	defer fs.mu.Unlock()
 
-	for i, d := range fs.data {
-		if d.ShortURL == urlData.ShortURL {
-			fs.data[i] = urlData
-			return fs.saveToFile()
-		}
-	}
-
-	fs.data = append(fs.data, urlData)
-	return fs.saveToFile()
+	fs.data[data.ShortURL] = data
+	return fs.saveToFile(data)
 }
 
 func (fs *FileStorage) Get(shortID string) (string, error) {
 	fs.mu.Lock()
 	defer fs.mu.Unlock()
 
-	for _, d := range fs.data {
-		if d.ShortURL == shortID {
-			return d.OriginalURL, nil
-		}
+	if data, exists := fs.data[shortID]; exists {
+		return data.OriginalURL, nil
 	}
 
 	return "", fmt.Errorf("URL not found")
