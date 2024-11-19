0e47c0c86921bc4abf0a5945294fabdc71d6d4d1
ilya <sol1kek@estelink.ru>
2024-11-19 23:42:21 +0300

    fix

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -44,7 +44,7 @@ func BenchmarkHandleJSONPost(b *testing.B) {
 
 		HandleJSONPost(w, req)
 
-		if response.StatusCode != http.StatusCreated && response.StatusCode != http.StatusConflict && response.StatusCode != http.StatusOk {
+		if response.StatusCode != http.StatusCreated && response.StatusCode != http.StatusConflict && response.StatusCode != http.StatusOK {
 			b.Errorf("Unexpected status code: %d", response.StatusCode)
 		}
 
